948a66817edc7a1985a425e639f436fabc8d950d
Duy Nguyen Ngoc <duynn@nal.vn>
2023-05-22 15:44:20 +0700

    fix: name model

--- server/api.py
+++ server/api.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routes import router as ToeicRouter
 
+origins = ["*"]
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/", tags=["Root"])
 async def read_root() -> dict:
